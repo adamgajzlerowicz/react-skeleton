7f4c7c09d55fb4100e27387a44e80052b76fc251
adamgajzlerowicz <adam.gajzlerowicz@performancehorizon.com>
2016-07-12 14:07:52 +0200

    router working ok

--- index.jsx
+++ index.jsx
@@ -1,12 +1,53 @@
 require("./node_modules/bootstrap/dist/css/bootstrap.min.css")
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, browserHistory } from 'react-router';
+import { Router, Route, Link, browserHistory} from 'react-router';
 
+var Container = React.createClass({
+    render() {
+        return <div class="container">
+            <Link to='foo' >Link1</Link>
+            <Link to='bar' >Link2</Link>
+
+            {this.props.children}
+        </div>
+    }
+});
+var Child1 = React.createClass({
+	render(){
+		return  (
+			<div>
+                Child 1
+			</div>
+		)
+	}
+});
+
+var Child2 = React.createClass({
+	render(){
+		return  (
+			<div>
+				Child 2
+			</div>
+		)
+
+	}
+});
+
+var routes = (
+	<Router history={browserHistory}>
+
+        <Route path="/" component={Container}>
+            <Route path="foo" component={Child2}/>
+            <Route path="bar" component={Child1} />
+        </Route>
+
+    </Router>
+);
 
 export class App extends React.Component {
 	render() {
-        let part_message = 'foo bar blah'
+		let part_message = 'foo bar blah';
 		let message = `${part_message}`;
 		return (
 			<div>{message}</div>
@@ -14,4 +55,4 @@ export class App extends React.Component {
 	}
 }
 
-ReactDOM.render(<App/>, document.querySelector("#myApp"));
+ReactDOM.render(routes, document.querySelector("#myApp"));
